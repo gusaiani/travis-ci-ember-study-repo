d310be2feccd9f0d297dab1a511a67418645f1bb
Gustavo Saiani <gustavo@poe.ma>
2016-12-09 09:46:23 -0200

    Add acceptance tests

--- tests/acceptance/builds/current-tab-test.js
+++ tests/acceptance/builds/current-tab-test.js
@@ -43,6 +43,22 @@ test('renders most recent repository and most recent build when builds present',
   });
 
   andThen(function () {
+    // TODO: This shouldn't be necessary. The cause for this test's
+    // unreliability is that we assert before the build information has been
+    // resolved. I'm actually not sure how this ever worked before.
+    assert.ok(currentRepoTab.showsCurrentBuild, 'Shows current build');
 
-  })
+    assert.ok(jobTabs.logTab.isShowing, 'Displays the log');
+    assert.ok(jobTabs.configTab.isHidden, 'Job config is hidden');
+  });
+
+  jobTabs.configTab.click();
+
+  andThen(function () {
+    assert.equal(jobTabs.configTab.contents, '{ \"language\": \"Hello\" }', 'config output is correct');
+    assert.ok(jobTabs.configTab.isShowing, 'Displays the job config');
+    assert.ok(jobTabs.logTab.isHidden, 'Job log is hidden');
+  });
+
+  percySnapshot(assert);
 });
